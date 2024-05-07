807f67135bcf36d78c264de29c296391dfbab519
Migueldsbatista <msb2@cesar.school>
2024-05-07 10:35:21 -0300

    agora temos raquetes

---
+++ enum.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+
+typedef enum {
+    RED,    // 0
+    GREEN,  // 1
+    BLUE    // 2
+} Color;
+
+typedef enum {
+    JAN = 1,
+    FEB = 2,
+    MAR = 3,
+    // ...
+    DEC = 12
+} Month;
+
+int main() {
+    Color myColor = RED;  // Usa o enum para definir uma cor
+    printf("My color is: %d\n", myColor);
+    Month myMonth = MAR;
+    printf("My month is: %d\n", myMonth);
+    return 0;
+}
--- src/main.c
+++ src/main.c
@@ -33,12 +33,12 @@ int incX = 1, incY = 1;  // Incrementos para mover o elemento na tela
 
 void printHello(int nextX, int nextY) {
     screenSetColor(CYAN, DARKGRAY);  // Define as cores de texto e fundo
-    screenGotoxy(x, y);  // Move o cursor para a posição (x, y)
-    printf("           ");  // Limpa o texto anterior
+    //screenGotoxy(x, y);  Move o cursor para a posição (x, y)
+    //printf("           ");  Limpa o texto anterior
     x = nextX;  // Atualiza a posição x
     y = nextY;  // Atualiza a posição y
     screenGotoxy(x, y);  // Move o cursor para a nova posição
-    printf("*");  // Imprime um caractere na nova posição (originalmente um HelloWorld)
+    printf("o");  // Imprime um caractere na nova posição (originalmente um HelloWorld)
 }
 
 void printKey(int ch) {
@@ -47,7 +47,7 @@ void printKey(int ch) {
     printf("Key code :");
 
     screenGotoxy(34, 23);  // Move o cursor para posição onde o código é mostrado
-    printf("            ");  // Limpa o texto anterior
+    printf("    ");  // Limpa o texto anterior
 
     if (ch == 27)  // Se o código for o de "Esc", ajusta a posição
         screenGotoxy(36, 23);
@@ -59,7 +59,62 @@ void printKey(int ch) {
         printf("%d ", readch());  // Imprime o código dos caracteres adicionais
     }
 }
+int raqueteDireitaY = 10;
+int raqueteEsquerdaY=10;  // Posição inicial da raquete
 
+void moverRaqueteDireitaParaCima() {
+    if (raqueteDireitaY > MINY + 2) {  // Verifica se não está no limite superior
+        raqueteDireitaY--;  // Move para cima
+        screenGotoxy(MAXX-2, raqueteDireitaY);  // Move cursor para a posição da raquete
+        printf("|");    
+        screenGotoxy(MAXX-2, raqueteDireitaY+1);
+        printf("|");
+        screenGotoxy(MAXX-2, raqueteDireitaY+2);
+        printf(" ");
+        screenUpdate();  // Atualiza a tela
+    }
+}
+
+void moverRaqueteDireitaParaBaixo() {
+    if (raqueteDireitaY < MAXY - 2) {  // Verifica se não está no limite inferior
+        raqueteDireitaY++;  // Move para baixo
+        screenGotoxy(MAXX-2, raqueteDireitaY);  // Move cursor para a posição da raquete
+        printf("|");
+        screenGotoxy(MAXX-2, raqueteDireitaY-1);
+        printf("|"); // Imprime a raquete
+        screenGotoxy(MAXX-2, raqueteDireitaY-2);
+        printf(" ");
+        screenUpdate();  // Atualiza a tela
+    }
+}
+
+//Raquetes arrow up arrow down
+void moverRaqueteEsquerdaParaCima() {
+    if (raqueteEsquerdaY > MINY + 2) {  // Verifica se não está no limite superior
+        raqueteEsquerdaY--;  // Move para cima
+        screenGotoxy(2, raqueteEsquerdaY);  // Move cursor para a posição da raquete
+        printf("|");
+        screenGotoxy(2, raqueteEsquerdaY+1);
+        printf("|");
+        screenGotoxy(2, raqueteEsquerdaY+2); // Move cursor para a posição da raquete
+        printf(" ");
+        screenUpdate();
+    }
+}
+
+void moverRaqueteEsquerdaParaBaixo() {
+    if (raqueteEsquerdaY < MAXY - 2) {  // Verifica se não está no limite inferior
+        raqueteEsquerdaY++;  // Move para baixo
+        screenGotoxy(2, raqueteEsquerdaY);  // Move cursor para a posição da raquete
+        printf("|");
+        screenGotoxy(2, raqueteEsquerdaY-1);
+        printf("|");  
+        screenGotoxy(2, raqueteEsquerdaY-2); // Move cursor para a posição da raquete
+        printf(" "); 
+
+        screenUpdate();  // Atualiza a tela
+    }
+}
 int main() {
     static int ch = 0;  // Variável para armazenar o código da tecla pressionada
 
@@ -74,17 +129,31 @@ int main() {
 
     // Loop principal do programa
     while (ch != 10) {  // Continua até que "Enter" seja pressionado
+        
+       
         // Manipulação da entrada do usuário
         if (keyhit()) {  // Se uma tecla foi pressionada
-            ch = readch();  // Lê o caractere pressionado
-            printKey(ch);  // Mostra o código da tecla
+            ch = readch();
+            if (ch == 119) {  // Se a tecla for 'W'
+                moverRaqueteEsquerdaParaCima();  // Move a raquete para cima
+            } else if (ch == 115) {  // Se a tecla for 'S'
+                moverRaqueteEsquerdaParaBaixo();  // Move a raquete para baixo
+            }  // Lê o caractere pressionado
+
+             if (ch == 105) {  // Se a tecla for 'W'
+                moverRaqueteDireitaParaCima();  // Move a raquete para cima
+            } else if (ch == 107) {  // Se a tecla for 'S'
+                moverRaqueteDireitaParaBaixo();  // Move a raquete para baixo
+            }  // Lê o caractere pressionado
+            printKey(ch);
+              // Mostra o código da tecla
             screenUpdate();  // Atualiza a tela
         }
 
         // Atualiza o estado do jogo
         if (timerTimeOver() == 1) {  // Verifica se o temporizador terminou
             int newX = x + incX;  // Calcula a nova posição no eixo X
-            if (newX >= (MAXX - strlen("*") - 1) || newX <= MINX + 1) 
+            if (newX >= (MAXX - 1) || newX <= MINX + 1) 
                 incX = -incX;  // Inverte a direção se atingir o limite
 
             int newY = y + incY;  // Calcula a nova posição no eixo Y
