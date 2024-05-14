84fb4dec62b3100e627982b73a76fa5417eb5ddf
Tico_Notebook <tgafa@cesar.school>
2024-05-14 08:48:40 -0300

    Refactor main.c file to remove unnecessary comments and author information

--- src/main.c
+++ src/main.c
@@ -3,12 +3,6 @@
  * Created on Aug, 23th 2023
  * Author: Tiago Barros
  * Based on "From C to C++ course - 2002"
-*/
-/**
- * main.h
- * Created on Aug, 23th 2023
- * Author: Tiago Barros
- * Based on "From C to C++ course - 2002"
  * 
  * 
  * 
